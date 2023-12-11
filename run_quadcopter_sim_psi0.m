07353368b72b9391f416e3087a41f5046aeb7b68
Victor-YG <yizhougao8@gmail.com>
2023-12-10 23:04:37 -0500

    Added implementation for psi = 0.

---
+++ run_quadcopter_sim_psi0.m
@@ -0,0 +1,90 @@
+clc
+clear
+clear all
+
+sim("multirotor_full_system_psi0.slx");
+t = ans.position.Time;
+
+x = ans.position.Data(:, 1);
+y = ans.position.Data(:, 2);
+z = ans.position.Data(:, 3);
+
+u = ans.velocity.Data(:, 1);
+v = ans.velocity.Data(:, 2);
+w = ans.velocity.Data(:, 3);
+
+phi = ans.attitude.Data(:, 1);
+theta = ans.attitude.Data(:, 2);
+psi = ans.attitude.Data(:, 3);
+
+phi_c = ans.control.Data(:, 1);
+theta_c = ans.control.Data(:, 2);
+psi_c = ans.control.Data(:, 3);
+w_c = ans.control.Data(:, 4);
+
+figure
+subplot(4, 1, 1)
+plot(t, phi_c)
+legend("phi_c")
+grid on
+
+subplot(4, 1, 2)
+plot(t, theta_c)
+legend("theta_c")
+grid on
+
+subplot(4, 1, 3)
+plot(t, psi_c)
+legend("psi_c")
+grid on
+
+subplot(4, 1, 4)
+plot(t, w_c)
+legend("w_c")
+grid on
+
+figure
+subplot(3, 3, 1)
+plot(t, x)
+legend("x")
+grid on
+
+subplot(3, 3, 2)
+plot(t, y)
+legend("y")
+grid on
+
+subplot(3, 3, 3)
+plot(t, z)
+legend("z")
+grid on
+
+subplot(3, 3, 4)
+plot(t, u)
+legend("u")
+grid on
+
+subplot(3, 3, 5)
+plot(t, v)
+legend("v")
+grid on
+
+subplot(3, 3, 6)
+plot(t, w)
+legend("w")
+grid on
+
+subplot(3, 3, 7)
+plot(t, theta)
+legend("theta")
+grid on
+
+subplot(3, 3, 8)
+plot(t, phi)
+legend("phi")
+grid on
+
+subplot(3, 3, 9)
+plot(t, psi)
+legend("psi")
+grid on
